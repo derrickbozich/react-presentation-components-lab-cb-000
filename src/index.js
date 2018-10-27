943c9170738fba3aaa6e29d95eae32ae0b1b4fe2
derrick b <dbozich1@gmail.com>
2018-10-27 21:20:35 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={() => "Oops, I did it again!"} />
+    <SimplerComponent handleClick={onClick={() => "Oops, I did it again!" } />
   </div>,
   document.getElementById('root')
 );
